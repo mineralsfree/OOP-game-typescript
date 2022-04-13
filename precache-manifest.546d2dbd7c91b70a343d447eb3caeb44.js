e6835e822b504db1b66016027de09e3491547952
Mikita Pilinka <mineralsfree@gmail.com>
2022-04-13 14:13:22 +0200

    Updates

---
+++ precache-manifest.546d2dbd7c91b70a343d447eb3caeb44.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "590a63dcaa3520d1c4d11c975a4ee23a",
+    "url": "/OOP-game-typescript/index.html"
+  },
+  {
+    "revision": "d033fd81c595cf9394bb",
+    "url": "/OOP-game-typescript/static/css/main.d61ab80c.chunk.css"
+  },
+  {
+    "revision": "ffa25a19f193ff068b5c",
+    "url": "/OOP-game-typescript/static/js/2.1ed504dc.chunk.js"
+  },
+  {
+    "revision": "d033fd81c595cf9394bb",
+    "url": "/OOP-game-typescript/static/js/main.7d9742c6.chunk.js"
+  },
+  {
+    "revision": "2feed1652c24555df180",
+    "url": "/OOP-game-typescript/static/js/runtime~main.3edffa7d.js"
+  },
+  {
+    "revision": "b90a84e1db6aea42431c3476258d8859",
+    "url": "/OOP-game-typescript/static/media/Archor.b90a84e1.png"
+  },
+  {
+    "revision": "4b38a3dfa4cac099b7091ea41263b196",
+    "url": "/OOP-game-typescript/static/media/Berserk.4b38a3df.png"
+  },
+  {
+    "revision": "36a5096358ca057fb39ef1af68b8fcac",
+    "url": "/OOP-game-typescript/static/media/Hexer.36a50963.png"
+  },
+  {
+    "revision": "e2c7c691ec7ea8174b35f4495657016f",
+    "url": "/OOP-game-typescript/static/media/Mage.e2c7c691.png"
+  },
+  {
+    "revision": "90c3e463107988e616ec8965b994e82f",
+    "url": "/OOP-game-typescript/static/media/Silence.90c3e463.png"
+  },
+  {
+    "revision": "7249fcd7831729cc0df6950f635ad765",
+    "url": "/OOP-game-typescript/static/media/WitchDoctor.7249fcd7.png"
+  }
+]);
